a9500497741f8ca7b6c850849a6f515a1728af06
ykaro nunes <ykaroyuri572@gmail.com>
2023-10-22 14:46:35 -0300

    alterações no layout

---
+++ imc.js
@@ -0,0 +1,14 @@
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+const calcular = document.getElementById('calcular');
+const resultado = document.getElementById('resultado');
+
+/* ACIMA ESTA TODAS OS ID'S ATRIBUIDOS QUE SÃO USADOS COM DOM*/
+
+
+
+
+
+
+

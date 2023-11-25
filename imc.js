7ef91ae05918ef11e984b971a5e206e30091939e
ykaro nunes <ykaroyuri572@gmail.com>
2023-11-25 11:03:16 -0300

    Adicionado design responsivo e mundaças nas stacks

--- imc.js
+++ imc.js
@@ -1,26 +1,29 @@
-const alturaInput = document.getElementById('altura');
-const pesoInput = document.getElementById('peso');
-const res = document.getElementById("resultado")
+let nome = document.getElementById("nome");
+let altura = document.getElementById("altura");
+let peso = document.getElementById("peso");
+let res = document.getElementById("result");
+let botao = document.getElementById("botao");
 
-/* ACIMA ESTA TODAS OS ID'S ATRIBUIDOS QUE SÃO USADOS COM DOM*/
-function aqui() {
-  const altura = Number(alturaInput.value) / 100;
-  const peso = Number(pesoInput.value);
-  let IMC = Math.round(peso / (altura * altura))
+function CalcularIMC(e) {
+  e.preventDefault();
 
-  if (IMC < 17) {
-    res.innerHTML = `Seu IMC é ${IMC} Você esta muito abaixo do peso`
-  } else if (IMC >= 17 && IMC < 18.5) {
-    res.innerHTML = `Seu IMC é ${IMC} Abaixo do peso`
-  } else if (IMC >= 18.5 && IMC < 25) {
-    res.innerHTML = `Seu IMC é ${IMC} Peso normal`
-  } else if (IMC >= 25 && IMC < 30) {
-    res.innerHTML = `Seu IMC é ${IMC} Acima do peso`
-  } else if (IMC >= 30 && IMC < 35) {
-    res.innerHTML = `Seu IMC é ${IMC} Obesidade I`
-  } else if (IMC >= 35 && IMC < 40) {
-    res.innerHTML = `Seu IMC é ${IMC}Obesidade II (severa)`
-  } else if (IMC >= 40) {
-    res.innerHTML = `Seu IMC é ${IMC}Obesidade III (mórbida)`
+  let nomeValue = nome.value;
+  let pesoValue = Number(peso.value);
+  let alturaValue = Number(altura.value);
+  let IMC = pesoValue / ((alturaValue / 100) * (alturaValue / 100));
+
+  if (IMC < 18) {
+    res.innerHTML = `${nomeValue} seu IMC é ${IMC.toFixed(2)}`;
+  } else if (IMC >= 18.5 && IMC <= 24.9) {
+    res.innerHTML = `${nomeValue} o seu IMC é ${IMC.toFixed(2)} `;
+  } else if (IMC >= 25.0 && IMC <= 29.9) {
+    res.innerHTML = `${nomeValue} o seu IMC é ${IMC.toFixed(2)} `;
+  } else if (IMC >= 30.0 && IMC <= 34.9) {
+    res.innerHTML = `${nomeValue} o seu IMC é ${IMC.toFixed(2)} `;
+  } else if (IMC >= 35.0 && IMC <= 39.9) {
+    res.innerHTML = `${nomeValue} o seu IMC é ${IMC.toFixed(2)} `;
+  } else if (IMC >= 40.0) {
+    res.innerHTML = `${nomeValue} o seu IMC é ${IMC.toFixed(2)}`;
   }
 }
+botao.addEventListener("click", CalcularIMC);
